ae62a7fb03c4e1633f68983f260e9dc8bab57595
Joseph Judge <4339897+heyjoeway@users.noreply.github.com>
2022-12-30 19:06:25 -0500

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,3 +1,4 @@
+cd ~
 winget install --accept-package-agreements --accept-source-agreements -h --scope machine python3
 winget install --accept-package-agreements --accept-source-agreements -h --scope machine git.git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
