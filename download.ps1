efee447d69c96e85397ab6100394a66822e4b2c5
Joseph Judge <4339897+heyjoeway@users.noreply.github.com>
2024-10-07 18:40:23 -0400

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,6 +1,6 @@
 cd ~
-winget install --accept-package-agreements --accept-source-agreements -h --scope machine python3
-winget install --accept-package-agreements --accept-source-agreements -h --scope machine git.git
+winget install --accept-package-agreements --accept-source-agreements -h --source winget --scope machine python3
+winget install --accept-package-agreements --accept-source-agreements -h --source winget --scope machine git.git
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 python -m pip install git+https://github.com/heyjoeway/miniwindeploy
 git clone --recurse-submodules https://github.com/heyjoeway/joeydebloat
