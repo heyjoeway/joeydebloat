a5cb3cfe0506c5efc7006c84e869deb2dfb57d12
Joseph Judge <4339897+heyjoeway@users.noreply.github.com>
2025-01-30 21:06:59 -0500

    Update Ungoogled Chromium.bat

--- All/2 Programs/Ungoogled Chromium.bat
+++ All/2 Programs/Ungoogled Chromium.bat
@@ -1,1 +1,3 @@
 choco install ungoogled-chromium --accept-license -y
+copy "%~dp0initial_preferences" "C:\ProgramData\chocolatey\lib\ungoogled-chromium\tools\ungoogled-chromium\"
+powershell -Command "$s=(New-Object -COM WScript.Shell).CreateShortcut('C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Ungoogled Chromium.lnk');$s.TargetPath='C:\ProgramData\chocolatey\lib\ungoogled-chromium\tools\ungoogled-chromium\chrome.exe';$s.Save()"
