46f0c6f305f6cebe9e3b6035a5488d12d38d2038
Joseph <joe@jojudge.com>
2025-04-25 10:09:06 -0400

    Fucking ms-gamebar

--- All/4 Tweaks/Remove Xbox Game Bar app.bat
+++ All/4 Tweaks/Remove Xbox Game Bar app.bat
@@ -5,5 +5,20 @@ echo --- Remove "Xbox Game Bar" app
 PowerShell -ExecutionPolicy Unrestricted -Command "Get-AppxPackage 'Microsoft.XboxGamingOverlay' | Remove-AppxPackage"
 :: Mark 'Microsoft.XboxGamingOverlay' as deprovisioned to block reinstall during Windows updates.
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Appx\AppxAllUserStore\Deprovisioned\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\GameDVR" /v "AppCaptureEnabled" /t REG_DWORD /d "0" /f
-reg add "HKCU\System\GameConfigStore" /v "GameDVR_Enabled" /t REG_DWORD /d "0" /f
+
+:: https://www.reddit.com/r/Windows11/comments/zwmeew/is_there_a_way_to_disable_this_blasted_msgamebar/
+reg add HKCR\ms-gamebar /f /ve /d URL:ms-gamebar 2>&1 >''
+
+reg add HKCR\ms-gamebar /f /v "URL Protocol" /d "" 2>&1 >''
+
+reg add HKCR\ms-gamebar /f /v "NoOpenWith" /d "" 2>&1 >''
+
+reg add HKCR\ms-gamebar\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >''
+
+reg add HKCR\ms-gamebarservices /f /ve /d URL:ms-gamebarservices 2>&1 >''
+
+reg add HKCR\ms-gamebarservices /f /v "URL Protocol" /d "" 2>&1 >''
+
+reg add HKCR\ms-gamebarservices /f /v "NoOpenWith" /d "" 2>&1 >''
+
+reg add HKCR\ms-gamebarservices\shell\open\command /f /ve /d "\\"$env:SystemRoot\System32\systray.exe\"" 2>&1 >''
